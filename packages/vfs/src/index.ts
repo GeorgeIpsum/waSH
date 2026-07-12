4ad2f541f1b59470c53fc108920d9dc3f6e5954b
Ibrahim Saberi <georgeipsum@gmail.com>
2026-07-11 20:17:46 -0400

    fix(vfs): move conformance suite to @wash/vfs/conformance subpath export

--- packages/vfs/src/index.ts
+++ packages/vfs/src/index.ts
@@ -3,4 +3,3 @@ export * from "./types.js";
 export * from "./errors.js";
 export { ulid } from "./ulid.js";
 export { MemoryBackend } from "./backend/memory.js";
-export { runBackendConformance } from "./conformance/suite.js";
